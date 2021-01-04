26f302a09bf28b44ed81837c11c9954d89a1c4bb
Carter Olson <carterolson3@gmail.com>
2021-01-04 16:36:16 -0600

    Added some style changes to home including unified

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import Head from "next/head";
+
 import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
+
 import { getSortedPostsData } from "../lib/posts";
-import Link from "next/link";
-import Date from "../components/date";
-import NowPlaying from "../components/NowPlayingCard";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import ProjectCard from "../components/ProjectCard";
 export async function getStaticProps() {
@@ -17,10 +15,18 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData }) {
+  
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
   return (
     <Layout>
       <div className="grid grid-cols-1  justify-center items-start max-w-5xl mx-auto mb-16 relative">
-        <div className="ml-4 mb-16 font-semibold text-2xl sm:text-4xl ">
+        <div className={`ml-4 mb-16 font-semibold text-2xl sm:text-4xl transform ${
+              mounted
+                ? "translate-y-0 duration-1000 "
+                : "translate-y-full "
+            }`}>
           <h1 className="mb-2">Hello! I'm Carter</h1>
           <h1 className=" mb-2 text-green-400 dark:text-indigo-500 ">
             A Front End Developer
@@ -35,11 +41,11 @@ export default function Home({ allPostsData }) {
             layout="fill" // required
             objectFit="cover"
           />
-          <p className="invisible">
-            YOLOYOLOYOLOYOLOYOLOYOLOYOLOYOLOYOLOYOLOYOLOYOLOYOLOYOLOYOLOYOLOYOLOYOLOYOLOYOLOYOLO
-          </p>
+       
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-gray-300 mb-16 ml-2 sm:ml-8">
+        <div>
+        <h3 className="w-40 font-semibold text-3xl relative pb-2 md:ml-4 border-l-2 border-b-2 pl-2 mb-4">Who I Am</h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-white mb-16 ml-2 sm:ml-8">
           <p>
             Hello! My name is Carter welcome to my personal website. My journey
             to becoming a programmer started as I watched YouTube videos of
@@ -59,15 +65,24 @@ export default function Home({ allPostsData }) {
             and don’t be shy to reach out about any questions.
           </p>
         </div>
+        </div>
         <div className="mb-16">
-          <h3 className="font-semibold text-3xl relative pb-4">My Skills</h3>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
+          <h3 className="w-40 font-semibold text-3xl relative pb-2 md:ml-4 border-l-2 border-b-2 pl-2 mb-4">My Skills</h3>
+          <p className=" ml-2 md:w-1/2 md:ml-4 mb-8 ">Over the years I've gained these skills through personal projects and schooling. My career goal is 
+            to always be learning and to also share my knowledge with my co-workers. I'm grateful for everyone
+            who has taught me something and I want to pass that down as long as I can.
+          </p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
             <div className="grid grid-flow-row auto-rows-auto gap-2">
               <p>🌟 React</p>
               <p>🌟 Javascript</p>
             </div>
             <div className="grid grid-flow-row auto-rows-auto gap-2">
-              <p>🌟 Styled-Components & Tailwind</p>
+              <p>🌟 Styled-Components</p>
+              <p>🌟 Tailwind</p>
+            </div>
+            <div className="grid grid-flow-row auto-rows-auto gap-2">
+              <p>🌟 Firebase</p>
               <p>🌟 HTML & CSS</p>
             </div>
             <div className="grid grid-flow-row auto-rows-auto gap-2">
@@ -77,7 +92,7 @@ export default function Home({ allPostsData }) {
           </div>
         </div>
         <section className="mt-10">
-          <h3 className="text-xl font-bold">Current Projects</h3>
+        <h3 className="w-40 font-semibold text-3xl relative pb-2 md:ml-4 border-l-2 border-b-2 pl-2 mb-4">Projects</h3>
           {/* grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 just bg-gray-800 */}
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 justify-items-center mt-2">
             <ProjectCard
